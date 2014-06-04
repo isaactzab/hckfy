74c02e1a3721c592cf2df103b7b86f6297c430de
Isaac Tzab <isaactzab@gmail.com>
2014-06-04 17:36:00 -0500

    - Changes     - Added items to menu     - Added art

--- gui.py
+++ gui.py
@@ -1,10 +1,10 @@
 import wx
 
 TRAY_TOOLTIP = 'System Tray Demo'
-TRAY_ICON = 'hckfy.ico'
+TRAY_ICON = 'icon.ico'
 
 
-def create_menu_item(menu, label, func):
+def cpm(menu, label, func):
     item = wx.MenuItem(menu, -1, label)
     menu.Bind(wx.EVT_MENU, func, id=item.GetId())
     menu.AppendItem(item)
@@ -12,30 +12,43 @@ def create_menu_item(menu, label, func):
 
 
 class TaskBarIcon(wx.TaskBarIcon):
+    notify_title = None
+    menu
     def __init__(self):
         super(TaskBarIcon, self).__init__()
         self.set_icon(TRAY_ICON)
-        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+        # self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
 
     def CreatePopupMenu(self):
-        menu = wx.Menu()
-        create_menu_item(menu, 'Say Hello', self.on_hello)
-        menu.AppendSeparator()
-        create_menu_item(menu, 'Exit', self.on_exit)
-        return menu
+        self.menu = wx.Menu()
+        self.notify_title = cpm(self.menu, 'Now Playing', self.nowplaying)
+        cpm(self.menu, 'This is an Sponsor!!', self.dummy)
+        cpm(self.menu, 'Update Sponsors database', self.dummy)
+        self.menu.AppendSeparator()
+        cpm(self.menu, 'Play/Pause', self.dummy)
+        self.menu.AppendSeparator()
+        cpm(self.menu, 'Donate', self.dummy)
+        self.menu.AppendSeparator()
+        cpm(self.menu, 'Exit', self.on_exit)
+        self.notify_title.Enable(False)
+        return self.menu
 
     def set_icon(self, path):
         icon = wx.IconFromBitmap(wx.Bitmap(path))
         self.SetIcon(icon, TRAY_TOOLTIP)
 
-    def on_left_down(self, event):
-        print 'Tray icon was left-clicked.'
+    # def on_left_down(self, event):
+    #     print 'Tray icon was left-clicked.'
+    #     self.popup(self.menu)
 
-    def on_hello(self, event):
+    def nowplaying(self, event):
         print 'Hello, world!'
+        self.popup(self.menu)
 
     def on_exit(self, event):
         wx.CallAfter(self.Destroy)
+    def dummy(self, event):
+        pass
 
 
 def main():
